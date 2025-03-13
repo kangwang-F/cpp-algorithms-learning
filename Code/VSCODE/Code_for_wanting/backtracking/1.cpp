60c8c3ece52efe58aa9c5a84806376f9d826a933
kangwang-F <215322566@qq.com>
2025-03-13 21:42:03 +0800

    回溯学了一点点

---
+++ Code/VSCODE/Code_for_wanting/backtracking/1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<limits>
+/*回溯
+*/
+using namespace std;
+//1.组合[给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。]
+class Solution {
+    public:
+        vector<vector<int>> combine(int n, int k) {
+            
+        }
+    };
+int main(){
+    return 0;
+}
